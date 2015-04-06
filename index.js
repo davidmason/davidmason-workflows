eff7c142120740189abeda88da7935e407be3ee3
David Mason <drdmason@gmail.com>
2015-04-07 08:40:37 +1000

    Prompt for whether to run kinit.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
-var exec = require('simple-subprocess');
+var exec = require('simple-subprocess')
+  , prompt = require('prompt');
 
 // TODO make this a menu
 //      check what is used for the menu in workshopper et al.
@@ -8,7 +9,31 @@ var exec = require('simple-subprocess');
 
 // Items to deal with at the start of a work day:
 
-console.log('Make sure you are signed in to kerberos:');
-exec('kinit').on('exit', function () {
-  console.log('\n    All done  :)\n');
+var schema = {
+  properties: {
+    kinit: {
+      description: 'Run kinit?',
+      required: true,
+      pattern: /^([yY]([eE][sS])?|[nN][oO]?)$/,
+      message: 'y/N'
+    }
+  }
+};
+
+prompt.start();
+prompt.get(schema, function (err, result) {
+  if (result.kinit.charAt(0).toLowerCase() === 'y') {
+    console.log('Running kinit:');
+    // TODO if it fails, keep running or offering to run
+    //      until it is successful.
+    exec('kinit').on('exit', function () {
+      nextBit();
+    });
+  } else {
+    nextBit();
+  }
 });
+
+function nextBit () {
+  console.log('\n    All done  :)\n');
+}
--- test.js
+++ test.js
@@ -1,19 +1,50 @@
 var child_process = require('child_process'),
-    should = require('should');
+    should = require('should'),
+    stripAnsi = require('strip-ansi');
 
 describe('davidmason-workflow', function () {
   describe('printing', function () {
-    it('Should remind me to run kinit', function () {
+    it('Should ask to run kinit', function (done) {
       var program = child_process.spawn('./index.js', []);
+
+      var whichData = 0;
+
       program.stdout.on('data', function (data) {
-        data.toString().should.eql('Make sure you are signed in to kerberos:\n');
+        if (whichData === 0) {
+          stripAnsi(data.toString()).should.eql('prompt: Run kinit?:  ');
+          // Select 'no'
+          program.stdin.write('n\n', 'utf8');
+        }
+
+        if (whichData === 1) {
+          data.toString().should.eql('\n    All done  :)\n\n');
+          done();
+        }
+
+        whichData++;
       });
-      program.on('exit', function (exitCode) {
-        exitCode.should.eql(0);
-      });
-      program.on('error', function (err) {
-        should(err).not.exist;
-      });
+
+
+      // TODO None of the following events are fired. Figure out why.
+
+      // Test is done on normal exit, or error
+
+      // program.on('exit', function (exitCode) {
+      //   exitCode.should.eql(0);
+      //   done();
+      // });
+      // program.on('close', function (exitCode) {
+      //   exitCode.should.eql(0);
+      //   done();
+      // });
+      // program.on('disconnect', function () {
+      //   console.log('disconnect event');
+      //   done();
+      // });
+      // program.on('error', function (err) {
+      //   should(err).not.exist;
+      //   done();
+      // });
     });
   });
 });
